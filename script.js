9dad5aa2b2cd7879813644fe874003d5e4c78b96
moisesmengo <moises.lins@academico.ifrn.edu.br>
2021-05-24 00:00:04 -0300

    criei um slidezinho com ícones

---
+++ script.js
@@ -0,0 +1,17 @@
+const icons = document.querySelectorAll('.sectio-1-icons i')
+let i = 1
+
+setInterval(()=>{
+  i++
+  const icon = document.querySelector('.sectio-1-icons i.change')
+
+  icon.classList.remove('change')
+
+  if(i > icons.length){
+    icons[0].classList.add('change')
+    i = 1
+  }else{
+    icon.nextElementSibling.classList.add('change')
+  }
+
+}, 4000)
