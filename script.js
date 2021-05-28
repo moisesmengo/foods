c8f46686a88ab9ad5f386bb7068f5013d3909ede
moisesmengo <moises.lins@academico.ifrn.edu.br>
2021-05-28 17:13:59 -0300

    header

--- script.js
+++ script.js
@@ -15,3 +15,9 @@ setInterval(()=>{
   }
 
 }, 4000)
+
+document.querySelector('.menu').addEventListener('click', () =>{
+  document.querySelectorAll('.target').forEach(item => {
+    item.classList.toggle('change')
+  })
+})
